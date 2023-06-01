77391fe8b10948c4738532f4bbaaf61ca206ba0d
Kevin Eduardo <5313724+kevineduardo@users.noreply.github.com>
2023-06-01 19:23:17 -0300

    Dist

--- dist/session.d.ts
+++ dist/session.d.ts
@@ -1,13 +1,10 @@
-import type { AuthenticationCreds, SignalDataTypeMap } from '@adiwajshing/baileys';
-import { proto } from '@adiwajshing/baileys';
+import type { AuthenticationCreds, SignalDataTypeMap } from '@whiskeysockets/baileys';
 export declare function useSession(sessionId: string): Promise<{
     state: {
         creds: AuthenticationCreds;
         keys: {
-            get: (type: keyof SignalDataTypeMap, ids: string[]) => Promise<{
-                [key: string]: Uint8Array | import("@adiwajshing/baileys").KeyPair | {
-                    [jid: string]: boolean;
-                } | proto.Message.IAppStateSyncKeyData | import("@adiwajshing/baileys").LTHashState;
+            get: <T extends keyof SignalDataTypeMap>(type: T, ids: string[]) => Promise<{
+                [p: string]: SignalDataTypeMap[T];
             }>;
             set: (data: any) => Promise<void>;
         };
--- dist/shared.d.ts
+++ dist/shared.d.ts
@@ -1,5 +1,5 @@
 import type { PrismaClient } from '@prisma/client';
-import type { SocketConfig } from '@adiwajshing/baileys';
+import type { SocketConfig } from '@whiskeysockets/baileys';
 export declare function setPrisma(prismaClient: PrismaClient): void;
 export declare function setLogger(pinoLogger?: SocketConfig['logger']): void;
 export declare function usePrisma(): PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client").Prisma.RejectOnNotFound | import(".prisma/client").Prisma.RejectPerOperation | undefined>;
